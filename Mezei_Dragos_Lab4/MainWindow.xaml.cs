323aa7e4fc71c1d4722a1bd79b08e208acf94a06
paraZite <paraZite@DESKTOP-2U8TDJM>
2020-11-27 18:40:22 +0200

    Add project files.

---
+++ Mezei_Dragos_Lab4/MainWindow.xaml.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AutoLotModel;
+using System.Data.Entity;
+
+namespace Mezei_Dragos_Lab4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    enum ActionState
+    {
+        New,
+        Edit,
+        Delete,
+        Nothing
+    }
+
+    public partial class MainWindow : Window
+    {
+        ActionState action = ActionState.Nothing;
+        AutoLotEntitiesModel ctx = new AutoLotEntitiesModel();
+        CollectionViewSource customerViewSource;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            customerViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource")));
+            customerViewSource.Source = ctx.Customer.Local;
+            ctx.Customer.Load();
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.New;
+
+            btnCancel.IsEnabled = true;
+            btnSave.IsEnabled = true;
+            btnEdit.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+
+            BindingOperations.ClearBinding(firstNameTextBox, TextBox.TextProperty);
+            BindingOperations.ClearBinding(lastNameTextBox, TextBox.TextProperty);
+            BindingOperations.ClearBinding(custIdTextBox, TextBox.TextProperty);
+            BindingOperations.ClearBinding(contract_valueTextBox, TextBox.TextProperty);
+            BindingOperations.ClearBinding(contract_dateDatePicker, DatePicker.SelectedDateProperty);
+            Keyboard.Focus(contract_dateDatePicker);
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Edit;
+
+            btnCancel.IsEnabled = true;
+            btnSave.IsEnabled = true;
+            btnNew.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+            customerDataGrid.IsEnabled = false;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Delete;
+
+            btnCancel.IsEnabled = true;
+            btnSave.IsEnabled = true;
+            btnNew.IsEnabled = false;
+            btnEdit.IsEnabled = false;
+            customerDataGrid.IsEnabled = false;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Customer customer = null;
+            if (action == ActionState.New)
+            {
+                try
+                {
+                    customer = new Customer()
+                    {
+                        FirstName = firstNameTextBox.Text.Trim(),
+                        LastName = lastNameTextBox.Text.Trim(),
+                        Contract_value = Decimal.Parse(contract_valueTextBox.Text.Trim()),
+                        Contract_date = contract_dateDatePicker.SelectedDate
+                    };
+                    ctx.Customer.Add(customer);
+                    customerViewSource.View.Refresh();
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            else if (action == ActionState.Edit)
+            {
+                try
+                {
+                    customer = (Customer)customerDataGrid.SelectedItem;
+                    customer.FirstName = firstNameTextBox.Text.Trim();
+                    customer.LastName = lastNameTextBox.Text.Trim();
+                    customer.Contract_value = Decimal.Parse(contract_valueTextBox.Text.Trim());
+                    customer.Contract_date = contract_dateDatePicker.SelectedDate;
+                    ctx.SaveChanges();
+                }
+
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                customerViewSource.View.Refresh();
+                customerViewSource.View.MoveCurrentTo(customer);
+            }
+            else if (action == ActionState.Delete)
+            {
+                try
+                {
+                    customer = (Customer)customerDataGrid.SelectedItem;
+                    ctx.Customer.Remove(customer);
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                customerViewSource.View.Refresh();
+
+            }
+            btnNew.IsEnabled = true;
+            btnEdit.IsEnabled = true;
+            btnDelete.IsEnabled = true;
+            btnSave.IsEnabled = false;
+            btnCancel.IsEnabled = false;
+            btnNext.IsEnabled = true;
+            btnPrevious.IsEnabled = true;
+            customerDataGrid.IsEnabled = true;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Nothing;
+
+            btnNew.IsEnabled = true;
+            btnEdit.IsEnabled = true;
+            btnDelete.IsEnabled = true;
+            btnSave.IsEnabled = false;
+            btnCancel.IsEnabled = false;
+            btnNext.IsEnabled = true;
+            btnPrevious.IsEnabled = true;
+            customerDataGrid.IsEnabled = true;
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            customerViewSource.View.MoveCurrentToPrevious();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            customerViewSource.View.MoveCurrentToNext();
+        }
+    }
+}
